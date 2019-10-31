d99fc5ec8f79a24da1316f09f44e80956006b70b
Colin Woodward <woodwardwebdev@gmail.com>
2019-10-31 18:48:03 +0000

    added colorHelpers function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
+import { generatePalette } from "./ColorHelpers";
 
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Palette {...seedColors[2]} />
-    </div>
-  );
+class App extends Component {
+  constructor(props, ...rest) {
+    super(props, ...rest);
+    this.state = {};
+  }
+
+  render() {
+    console.log(generatePalette(seedColors[2]));
+    return (
+      <div className="App">
+        <Palette {...seedColors[2]} />
+      </div>
+    );
+  }
 }
 
 export default App;
